83dce2313704bc90dd9a2e72a52b98e70c08aafd
bstrat <benstratton100@gmail.com>
2023-04-11 14:59:07 -0500

    created basic components and router fuction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+// Components
+import Dashboard from "./components/Dash";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
-function App() {
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
   return (
-    <></>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/login"
+              element={
+                !isAuthenticated ? <Login setAuth={setAuth} /> : <Navigate to="/dashboard" />
+              }
+            />
+            <Route
+              exact
+              path="/signup"
+              element={
+                !isAuthenticated ? <Signup setAuth={setAuth} /> : <Navigate to="/dashboard" />
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              element={
+                isAuthenticated ? <Dashboard setAuth={setAuth} /> : <Navigate to="/login" />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
