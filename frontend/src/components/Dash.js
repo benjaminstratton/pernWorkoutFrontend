a6bd0c8374f8238af2719005dc6a9d599861db01
bstrat <benstratton100@gmail.com>
2023-04-11 20:18:55 -0500

    created logout function

--- frontend/src/components/Dash.js
+++ frontend/src/components/Dash.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Dashboard = ({setAuth}) => {
+
+    const logout = (e) => {
+        e.preventDefault()
+        localStorage.removeItem("token")
+        setAuth(false)
+    }
+
   return (
     <>
       <h1>Dashboard</h1>
-      <button onClick={() => setAuth(false)}>Logout</button>
+      <button className="btn btn-primary" onClick={e => logout(e)}>Logout</button>
     </>
   );
 };
