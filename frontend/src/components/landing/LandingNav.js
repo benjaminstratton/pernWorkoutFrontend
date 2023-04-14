787dc7abb78ead4483f5eef8c865df1ae32541a9
bstrat <benstratton100@gmail.com>
2023-04-14 17:22:24 -0500

    navbar for landing page complete

--- frontend/src/components/landing/LandingNav.js
+++ frontend/src/components/landing/LandingNav.js
@@ -4,6 +4,8 @@ import { HiOutlineBars3 } from "react-icons/hi2";
 import {
   Box,
   Drawer,
+  List,
+  Divider,
   ListItem,
   ListItemButton,
   ListItemIcon,
@@ -44,6 +46,21 @@ const LandingNav = () => {
       <div className="landing-navbar-menu-container">
         <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
       </div>
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box sx={{ width: 250 }} role="presentation" onClick={() => setOpenMenu(false)} onKeyDown={() => setOpenMenu(false)}>
+            <List>
+                {menuOptions.map((item) => (
+                    <ListItem key={item.text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+        </Box>
+      </Drawer>
     </nav>
   );
 };
