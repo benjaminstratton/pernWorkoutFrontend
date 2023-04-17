ba2a2a1ce16fbafe6360ad114b675aa9feae1578
bstrat <benstratton100@gmail.com>
2023-04-16 21:35:00 -0500

    got rid of warnings for production

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 // Components
 import Landing from "./components/landing/Landing";
